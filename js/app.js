a588ead8b5df4e3dcaa0316f8910333545ddd10e
Catarina Lima <106818359+catarinamsl@users.noreply.github.com>
2022-08-30 18:01:09 -0300

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -7,11 +7,18 @@ $(document).ready(function () {
     items: 1,
     dots: false,
     smartSpeed: 1000,
+    autoplay: true,
+    autoplayTimeout: 5000,
+    autoplayHoverPause: false,
     navText: ['PREV', 'NEXT'],
     responsive: {
-      0: {},
-      600: {},
-      1000: {},
+      0: {
+        nav: false,
+      },
+
+      768: {
+        nav: true,
+      },
     },
   });
 });
